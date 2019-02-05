da847edc5918da43029c2c37f5ee58ea2e72f2c5
bhflm <germanbonin@gmail.com>
2019-02-05 00:37:53 -0300

    nice refactor and add dank doggo pic

--- bot.py
+++ bot.py
@@ -1,14 +1,38 @@
 import smtplib
+import os
 from keys import *
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
 
-def funciona():
+def gedear():
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    msg = MIMEMultipart()
+    msg['From'] = userEmail
+    msg['To'] = emailTarget
+    msg['Subject'] = 'D A N K  D O G G O'
+    body = 'G O O D   B O I I I <3' 
+    msg.attach(MIMEText(body,'plain'))
+
+    filename = pic
+    attachment = open(filename,'rb')
+
+    # all the encoding bs for uploading just a simple jpg to an email with MIME
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+    #format this mf for email
+    msg.attach(part)
+    text = msg.as_string()
+
+    server = smtplib.SMTP(googleHost, googlePort)
     server.starttls()
-
     server.login(userEmail, userPassword)
-    i = 0 # i can make this better this is just a test i swear
-    server.sendmail(userEmail, emailTarget, "whatever the fuck")
+    server.sendmail(userEmail, emailTarget, text)
+    server.quit()
 
-funciona()
+gedear()
